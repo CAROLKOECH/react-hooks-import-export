83b4f7c6bf980e429cb2306cdcf68f3247603d83
CAROLKOECH <koechc44@gmail.com>
2023-07-18 20:11:36 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -2,7 +2,16 @@ import React from "react";
 import howManyParks from "./parks/howManyParks";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  // Call the function and store its result in a variable
+  const parksCount = howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  // Use the variable in the JSX to render the result along with the heading
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <p>{parksCount}</p>
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
